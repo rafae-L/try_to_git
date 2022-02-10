543b7cb0181d7f4344ddbe7a7cd1e363bc6c2018
rafae-L <khuzin.ri@phystech.edu>
2022-02-10 22:41:22 +0300

    Big changes

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <chrono>
+#include <cmath>
 
-using namespace std;
+using namespace std::chrono;
+
+class Timer {
+public:
+    Timer() {}
+    ~Timer() {
+        std::cout << duration_cast<std::chrono::microseconds>(steady_clock::now() - start).count() << std::endl;
+    }
+
+private:
+    steady_clock::time_point start = steady_clock::now();
+};
+
 
 int main() {
-    cout << "Hello, World!" << endl;
+    double result = 0.;
+    {
+        Timer t;
+        for(auto i = 0u; i < 10000000u; i++) {
+            result += std::sin(i) + std::cos(i);
+        }
+    }
+    std::cout << "Result is " << result;
     return 0;
 }
